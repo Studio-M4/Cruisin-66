a84f0605db7b13c65b638f9a7718cdafaf9d0137
Sophia Lee <soph915@gmail.com>
2018-08-17 19:45:15 -0700

    update package.json for testing

--- client/_tests_/CreateStop.test.js
+++ client/_tests_/CreateStop.test.js
@@ -4,14 +4,14 @@ import renderer from 'react-test-renderer';
 
 describe ('CreateStop component', () => {
     it ('renders correctly', () => {
-        // const props = {
-        //     photos: [], 
-        //     itineraryId: null, 
-        //     address: null,
-        //     description: null,
-        //     name: null,
-        // }
-        const tree = renderer.create(<CreateStop/>).toJSON();
+        const props = {
+            photos: [], 
+            itineraryId: null, 
+            address: null,
+            description: null,
+            name: null,
+        }
+        const tree = renderer.create(<CreateStop {...props}/>).toJSON();
         expect(tree).toMatchSnapshot();
     })
 })
