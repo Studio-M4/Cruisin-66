a9b9e92ce22b354a85443f8693496d87904940b7
Sophia Lee <soph915@gmail.com>
2018-08-17 17:32:08 -0700

    set up snapshot testing

--- client/_tests_/Profile.test.js
+++ client/_tests_/Profile.test.js
@@ -1,20 +1,11 @@
-import Profile from '../component/Profile.js';
 import React from 'react';
+import Profile from '../component/Profile.js';
 import renderer from 'react-test-renderer';
-import Enzyme, { shallow, render, mount } from 'enzyme';
 
-describe('Profile component', () => {
-  // it('Profile: renders correctly', () => {
-  //   const props = { selectedIndex: 0, user: {}}
-  //   const navigation = { navigate: jest.fn() };
-  //   const tree = renderer.create(<Profile navigation={navigation}  {...props}/> ).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
-
-
-  //error because 
-  it('Profile: renders correctly', () => {
-    const wrapper = shallow (<Profile/>)
-    expect (wrapper).toMatchSnapshot();
-  });
-});
+describe ('Profile component', () => {
+    it ('renders correctly', () => {
+        const tree = renderer.create(<Profile/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+})
+>>>>>>> set up snapshot testing
