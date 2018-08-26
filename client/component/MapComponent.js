c45b9cf8b94a26d860c65ab91df5575bedaa1233
Julio Jean Fils <julio.fils1@gmil.com>
2018-08-25 17:35:06 -0700

    last rebase

--- client/component/MapComponent.js
+++ client/component/MapComponent.js
@@ -31,7 +31,7 @@ import {
   Input
 } from "native-base";
 
-import MapView from "react-native-maps";
+import MapView   from "react-native-maps";
 import { Marker } from "react-native-maps";
 
 export default class CommentStop extends React.Component {
@@ -40,23 +40,42 @@ export default class CommentStop extends React.Component {
   };
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {},
+    region = {};
   }
+  getInitialState() {
+    return {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  onRegionChange = region => {
+    this.setState({ region });
+  }
+  
 
   render() {
     return (
       <View style={styles.container}>
+ 
         <MapView
           userLocationAnnotationTitle="me"
           followsUserLocation
           showsUserLocation
+          
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
           style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121
-          }}
+          // region={{
+          //   latitude: 37.78825,
+          //   longitude: -122.4324,
+          //   latitudeDelta: 0.015,
+          //   longitudeDelta: 0.0121
+          // }}
         >
           {[
             {
